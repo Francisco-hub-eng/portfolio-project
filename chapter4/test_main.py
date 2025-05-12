f034315775331ee009812129b2a8a9b2a3dc5ffa
Francisco Urra <urra.francisco@googlemail.com>
2025-05-12 17:57:21 +0000

    creating test_main

---
+++ chapter4/test_main.py
@@ -0,0 +1,35 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+# test the health check endpoint
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "API health check successful"}
+
+# test /v0/players/
+def test_read_players():
+    response = client.get("/v0/players/?skip=0&limit=10000")
+    assert response.status_code == 200
+    assert len(response.json()) == 1018
+
+def test_read_players_by_name():
+    response = client.get("/v0/players/?first_name=Bryce&last_name=Young")
+    assert response.status_code == 200
+    assert len(response.json())==1
+    assert response.json()[0].get("player_id") == 2009
+
+# test /v0/players/{player_id}/
+def test_read_players_with_id():
+    response = client.get("/v0/players/1001/")
+    assert response.status_code == 200
+    assert response.json().get("player_id") == 1001
+
+# test /v0/performances/
+def test_read_performances():
+    response = client.get("/v0/performances/?skip=0&limit=20000")
+    assert response.status_code == 200
+    assert len(response.json()) == 17306
+ 
