82d8a3cc9cda16c584beaa7a5eb411e09c6e3606
Francisco Urra <urra.francisco@googlemail.com>
2025-05-14 13:22:33 +0000

    typo swc_client

--- chapter7/sdk/src/swcpy/swc_client.py
+++ chapter7/sdk/src/swcpy/swc_client.py
@@ -3,7 +3,7 @@ import httpx
 class SWCClient:
 
     def __init__(self, swc_base_url : str):
-        self.swc_base_url : swc_base_url
+        self.swc_base_url = swc_base_url
 
     def get_health_check(self):
         # make the API call
